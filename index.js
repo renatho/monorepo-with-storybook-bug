9022d94a6ae1941c6f8136bad12701774a41f3f8
Renatho <renatho@gmail.com>
2018-12-28 13:54:53 -0200

    Add webpack

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Temp2 from './Temp2';
+
+const wrapper = document.getElementById("wrapper");
+
+render(
+  <Temp2 />,
+  wrapper,
+);
