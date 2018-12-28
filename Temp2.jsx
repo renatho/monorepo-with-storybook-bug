e0e791532ec3fe9c8dbca3a6200a3352cd195bf9
Renatho <renatho@gmail.com>
2018-12-28 10:29:12 -0200

    Version 1 working

---
+++ Temp2.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Dep1 from 'dep1';
+
+const Temp2 = () => <div>Hey! <Dep1 /></div>
+
+export default Temp2;
---
+++ packages/dep1/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dep1 = () => <div>Dep 1</div>;
+
+export default Dep1;
